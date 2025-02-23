f28a69449426ebce0779fcd121d8b94ae760f370
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-02-23 15:04:21 +0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: ['https://durak.xuton.uno', 'http://0.0.0.0:8000', 'http://0.0.0.0:3000', 'http://localhost:8000', 'http://localhost:3000'],
+    origin: [
+      'https://durak.xuton.uno',
+      'http://0.0.0.0:3000',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://0.0.0.0:5173',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+    ],
     credentials: true,
     methods: '*',
     allowedHeaders: ['*', 'Authorization', 'Content-Type']
